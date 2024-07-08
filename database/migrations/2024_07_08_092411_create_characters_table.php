35631d2b9f17636142b8f57ffbbaaffadb3fc850
reyhanasta <astareyhan@gmail.com>
2024-07-08 16:43:57 +0700

    Adding Tabels

---
+++ database/migrations/2024_07_08_092411_create_characters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('attribute');
+            $table->unsignedInteger('weapon');
+            $table->enum('rarity',[5,4,3]);
+            $table->enum('type',['limited','standard']);
+            $table->timestamps();
+
+
+            // $table->foreign('attribute')->references('id')->on('attributes');
+            // $table->foreign('weapon')->references('id')->on('weapon_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
